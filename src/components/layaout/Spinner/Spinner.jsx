2e6a5732b2e450871c7d5b9b1e39e2974bdeeb7e
Miller Andres Rocha Castano <miller.rocha@globant.com>
2022-05-19 12:21:07 -0500

    github finder

---
+++ src/components/layaout/Spinner/Spinner.jsx
@@ -0,0 +1,9 @@
+import './Spinner.css'
+
+const Spinner = () => {
+  return (
+    <span className="loader text-center mx-auto"></span>
+  )
+}
+
+export default Spinner
---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react"
+import Spinner from "../layaout/Spinner/Spinner"
+import UserItem from "./UserItem"
+import GithubContext from "../../context/github/GithubContext"
+
+const UserResults = () => {
+  const {users, loading } = useContext(GithubContext)
+
+  if(!loading){
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {
+          (users || []).map((user)=>(
+            <UserItem
+              key={user.id}
+              user={user}
+            />
+            ))
+          }
+      </div>
+    )
+  } else {
+    return (
+      <div className="grid grid-cols-1">
+        <Spinner/>
+      </div>
+    )
+  }
+
+}
+
+export default UserResults
